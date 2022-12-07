bf8b8c990a1692e584cfe0d32de2cec7bb701984
Bruno <navalsaas@gmail.com>
2022-12-07 16:54:37 -0300

    fix componentes html

--- src/components/Modals/Adicionar_objetivo.vue
+++ src/components/Modals/Adicionar_objetivo.vue
@@ -1,7 +1,11 @@
 <template>
     <h2>Adicionar Objetivo</h2>
                  <input type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" />
-                 <img src="https://via.placeholder.com/150">
+                 <select class="select select-bordered w-full max-w-xs">
+  <option disabled selected>Who shot first?</option>
+  <option>Han Solo</option>
+  <option>Greedo</option>
+</select>
                  <textarea class="textarea textarea-bordered" placeholder="Bio"></textarea>
     <div class="modal-action">
                      <label for="my-modal" class="btn">Adicionar objetivo</label>
