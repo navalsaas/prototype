122c92d63c2b8b4f62d0c9cf9411ea47db87f283
Bruno <navalsaas@gmail.com>
2022-12-09 13:58:31 -0300

    tarefa anual e mensal novo badge

--- src/components/Modals/Adicionar_tarefa_mensal_anual.vue
+++ src/components/Modals/Adicionar_tarefa_mensal_anual.vue
@@ -22,11 +22,20 @@
       </label>
       <input type="date" class="input input-bordered w-full" />
       <p class="mt-2">Voce escolheu os dias:
-      <div class="badge badge-primary mr-1">23/03</div>
+      <div class="badge badge-info gap-2 mr-1">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-4 h-4 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path></svg>
+      23 de jan
+      </div>
       
-      <div class="badge badge-primary mr-1">23/03</div>
+      <div class="badge badge-info gap-2 mr-1">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-4 h-4 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path></svg>
+      23 de jan
+      </div>
       
-      <div class="badge badge-primary mr-1">23/03</div>
+      <div class="badge badge-info gap-2 mr-1">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-4 h-4 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path></svg>
+      23 de jan
+      </div>
       </p>
     </div>
     <div class="modal-action">
